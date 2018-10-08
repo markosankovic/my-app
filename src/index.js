c9646a3df318095044fbc334d76f5b74aed92896
Marko Sanković <msankovic@synapticon.com>
2018-10-08 11:54:47 +0200

    Forms

--- src/index.js
+++ src/index.js
@@ -8,16 +8,23 @@ import LogingControl from './LoginControl';
 import Mailbox from './Mailbox';
 import Page from './Page';
 import NumberList from './NumberList';
+import NameForm from './NameForm';
+import EssayForm from './EssayForm';
+import FlavorForm from './FlavorForm';
 import * as serviceWorker from './serviceWorker';
+import Reservation from './Reservation';
 
 ReactDOM.render(<LogingControl />, document.getElementById('loginControl'));
 ReactDOM.render(<Page />, document.getElementById('page'));
-ReactDOM.render(<Mailbox unreadMessages={['React', 'Re: React', 'Re:Re: React']}/>, document.getElementById('mailbox'));
+ReactDOM.render(<Mailbox unreadMessages={['React', 'Re: React', 'Re:Re: React']} />, document.getElementById('mailbox'));
 ReactDOM.render(<Clock />, document.getElementById('clock'));
 ReactDOM.render(<Toggle />, document.getElementById('toggle'));
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<NumberList numbers={[1, 3, 5, 7]} />, document.getElementById('numberList'));
-
+ReactDOM.render(<NameForm />, document.getElementById('nameForm'));
+ReactDOM.render(<EssayForm />, document.getElementById('essayForm'));
+ReactDOM.render(<FlavorForm />, document.getElementById('flavorForm'));
+ReactDOM.render(<Reservation />, document.getElementById('reservation'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
